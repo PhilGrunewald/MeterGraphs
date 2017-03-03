41cecd18b98638e9ca3032a94a9b80ec3c0a5a11
Phil Grunewald <philipp.grunewald@ouce.ox.ac.uk>
2017-03-03 11:56:38 +0000

    user message

--- index.php
+++ index.php
@@ -18,12 +18,102 @@
     if(isset($_GET['id'])){ $id = $_GET['id']; }
     echo '<script>var hhid = "'.$id.'";</script>';
 	include('_nav_bar_yourdata.php');
+	include('../db.php');
+	$db = mysqli_connect($server,$dbUserName,$dbUserPass,$dbName);
+
+   $sqlq = "SELECT idMeta FROM Meta WHERE Household_idHousehold = '" . $id . "' AND DataType = 'E'";
+   $q_idMeta = mysqli_query($db,$sqlq);
+   $f_idMeta = mysqli_fetch_assoc($q_idMeta);
+   $idMeta   = $f_idMeta['idMeta'];
+   
+   $fromEl = "FROM Electricity_1min WHERE Watt > 20 AND Meta_idMeta = " . $idMeta;
+   $fromAll = "FROM Electricity_1min WHERE Watt > 20"; 
+
+   // HH Peak 
+   $sqlq = "SELECT dt,Watt " . $fromEl . " ORDER BY Watt DESC LIMIT 1";
+   $q = mysqli_query($db,$sqlq);
+   $f = mysqli_fetch_assoc($q);
+   $dtPeak = $f['dt'];
+   $peak   = round($f['Watt']);
+
+   // HH Average
+   $sqlq = "SELECT AVG(Watt) as mean " . $fromEl ;
+   $q = mysqli_query($db,$sqlq);
+   $f = mysqli_fetch_assoc($q);
+   $mean = round($f['mean']);
+
+   // HH Baseload
+   $sqlq = "SELECT dt,Watt " . $fromEl . " ORDER BY Watt ASC LIMIT 1";
+   $q = mysqli_query($db,$sqlq);
+   $f = mysqli_fetch_assoc($q);
+   $dtBaseload = $f['dt'];
+   $baseload   = round($f['Watt']);
+
+
+   // Peak all
+   // $sqlq = "SELECT AVG(peakloads) as peakAll FROM peakloads";
+   $sqlq = " SELECT AVG(peakloads) as peakAll FROM (   SELECT MAX(`Electricity_1min`.`Watt`) AS `peakloads` FROM `Electricity_1min` WHERE (`Electricity_1min`.`Watt` > 20) GROUP BY `Electricity_1min`.`Meta_idMeta`) as a";
+   $q = mysqli_query($db,$sqlq);
+   $f = mysqli_fetch_assoc($q);
+   $peakAll = round($f['peakAll']);
+
+   // Average all
+   $sqlq = "SELECT AVG(Watt) as meanAll " . $fromAll ;
+   $q = mysqli_query($db,$sqlq);
+   $f = mysqli_fetch_assoc($q);
+   $meanAll = round($f['meanAll']);
+
+   // Baseload all
+   $sqlq = " SELECT AVG(baseloads) as baseloadAll FROM (   SELECT MIN(`Electricity_1min`.`Watt`) AS `baseloads` FROM `Electricity_1min` WHERE (`Electricity_1min`.`Watt` > 20) GROUP BY `Electricity_1min`.`Meta_idMeta`) as a";
+   $q = mysqli_query($db,$sqlq);
+   $f = mysqli_fetch_assoc($q);
+   $baseloadAll = round($f['baseloadAll']);
+
   ?>
 <div class="container">
  <div class="row">
   <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2" style="background-color: transparent;">
 
   <h3>Your electricity profile</h3>
+  <p>Thank you for contributing your data to this study. Here is your load profile. </p>
+<p>
+Your <b>average</b> use has been <b><?php echo $mean; ?> Watt</b>, compared to <?php echo $meanAll; ?> W across our participants. These values vary quite a bit day by day and depend on the type of your appliances, season and many other factors (which is part of what we try to understand with this study).</p>
+
+<p>Your <b>baseload</b> is perhaps more informative. The lowest reading over these 28 hours was 
+<?php echo "<b>".$baseload; 
+	if ($baseload > $baseloadAll) {
+		echo " W</b>. This is higher than the average ($baseloadAll W)";
+		if ($baseload < (1.4 * $baseloadAll)) {
+			echo ", but not by much. ";
+		} else {
+			echo ". ";
+		}
+	} else {
+		echo " W</b>. You have done better than our average participant ($baseloadAll W). ";
+	}
+?>
+This baseload mostly depends on stand-by devices and things that are always on (fridges, broadband routers...)</p>
+
+<p>The highest recorded electricity use occurred at <?php echo $peaktime; ?> (<?php echo $peak; ?> W). Which appliances do you think might have been on at this point?
+</p>
+
+<form class="form-inline" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
+<input type="hidden" id="id" name="id" value="<?php echo $id; ?>">
+<select name="Appliance">
+  <option value="None">Don't know</option>
+  <option value="Oven">Oven</option>
+  <option value="Microwave">Microwave</option>
+  <option value="EV">Electric Vehicle</option>
+  <option value="Dishwasher">Dish washer</option>
+  <option value="Washingmachine">Washing machine</option>
+  <option value="Tumble Dryer">Tumble dryer</option>
+  <option value="Other">Other</option>
+</select>
+</form>
+<p>
+
+ (Just pick the most important, if there were more than one.</p>
+
   <div id="canvas"></div>
   <div class='tooltip'></div>
 
